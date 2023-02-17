ba84be333fa018084ec8fa14a188f87d05a2a31c
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-17 09:08:24 +0500

    in progress

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import styles from './Content.module.css'
 import dickSucker from './imgs/dickSucker.png'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Messages from "../Messages/Messages";
+import Profile from '../Profile/Profile'
+
+function Home(){ return <h2>Главная</h2>; }
 
 export default function Content() {
   return (
-        <div className={styles.wrapper}>
-            <img className={styles.dickSucker} src={dickSucker} alt="dickSucker"></img>
-        </div>
+        <BrowserRouter>
+            <div className={styles.wrapper}>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Messages}/>
+                </Routes>
+                <img className={styles.dickSucker} src={dickSucker} alt="dickSucker"></img>
+            </div>
+        </BrowserRouter>
   )
 }
--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import styles from './SideMenu.module.css'
 import menuIcon from './imgs/menuIcon.png'
-import {Link} from 'react-router-dom'
+import Profile from '../Profile/Profile'
+import Messages from '../Messages/Messages'
+import {Link, BrowserRouter} from 'react-router-dom'
 
 export default function SideMenu() {
   return (
-    <div className={styles.wrapper}>
-        <div className={styles.desktop}>
-            <ul className={styles.ul}>
-                <li>Моя страница</li>
-                <li>Сообщения</li>
-                <li>Фото</li>
-            </ul>
-        </div>
-        <div className={styles.mobile}>
-            <img src={menuIcon}></img>
-            {/* <ul className={`${styles.menu} ${mobile ? '' : '.hidden'}`}>
+      <BrowserRouter>
+            <div className={styles.wrapper}>
+                <div className={styles.desktop}>
+                    <div><Link to={'/'}>main</Link></div>
+                    <div><Link to={'/profile'}>profile</Link></div>
+                    <div><Link to={'/messages'}>messages</Link></div>
+                </div>
+                <div className={styles.mobile}>
+                    <img src={menuIcon}></img>
+                    {/* <ul className={`${styles.menu} ${mobile ? '' : '.hidden'}`}>
                 <li><a href='/footer'>Footer</a></li>
                 <li><a href='/header'>Header</a></li>
                 <li>3</li>
                 <li>4</li>
                 <li>5</li>
             </ul> */}
-        </div>
-    </div>
+                </div>
+            </div>
+      </BrowserRouter>
   )
 }
