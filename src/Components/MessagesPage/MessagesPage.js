10ec5ae84e1346ad6e768a8e7c4042d8a5c3ee5e
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-20 21:13:11 +0500

    refactored prop dialogId

--- src/Components/MessagesPage/MessagesPage.js
+++ src/Components/MessagesPage/MessagesPage.js
@@ -12,10 +12,10 @@ const MessagesPage = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.dialogs}>
-                <DialogItem name={'Dima'} id={0}></DialogItem>
-                <DialogItem name={'Eugene'} id={1}></DialogItem>
-                <DialogItem name={'Sasha'} id={2}></DialogItem>
-                <DialogItem name={'xx'} id={3}></DialogItem>
+                <DialogItem name={'Dima'} dialogId={0}></DialogItem>
+                <DialogItem name={'Eugene'} dialogId={1}></DialogItem>
+                <DialogItem name={'Sasha'} dialogId={2}></DialogItem>
+                <DialogItem name={'xx'} dialogId={3}></DialogItem>
             </div>
             <div className={styles.messages}>
                 <Message text={'hi'}></Message>
