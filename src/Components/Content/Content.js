867ada9deba249c7f40dc611141ff6aa738d791f
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-20 21:09:18 +0500

    added components for dialogItem and Message

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Content.module.css'
 import {Route, Routes} from "react-router-dom";
-import Messages from '../Messages/Messages';
+import MessagesPage from '../MessagesPage/MessagesPage';
 import Profile from '../Profile/Profile'
 
 function Home(){ return <h2>Главная</h2>; }
@@ -12,7 +12,7 @@ export default function Content() {
           <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/profile' element={<Profile/>}/>
-              <Route path='/messages' element={<Messages/>}/>
+              <Route path='/messages' element={<MessagesPage/>}/>
           </Routes>
       </div>
   )
