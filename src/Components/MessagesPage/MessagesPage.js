83b824528bcd09cc8eb9c63bafc88e76690b4869
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-24 15:21:44 +0500

    mapping data for pattern elements

--- src/Components/MessagesPage/MessagesPage.js
+++ src/Components/MessagesPage/MessagesPage.js
@@ -3,24 +3,41 @@ import styles from './MessagesPage.module.css'
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
 
-let data = {
-    "Dima": ["Hi", "it's me"],
-    "Eugene": ["Hi", "what's up?"],
+let dialogsData = [
+    { id: 0, name: "Dima"},
+    { id: 1, name: "Eugene"},
+]
+let messagesData = [
+    { id: 0, name: "Dima", messages: ["Hi", "it's me"] },
+    { id: 1, name: "Eugene", messages: ["Hi", "what's up?"] },
+]
+let dialogs = dialogsData.map(d => <DialogItem className={styles.dialog} name={d.name} id={d.id}/>)
+let active;
+const chooseActiveDialog = () => {
+    let dialogs = document.getElementsByClassName('.dialog');
+    for (const dialog of dialogs) {
+        if (dialog.className.includes('active'))
+            active = dialog;
+    }
+}
+const messages = () => {
+    let activeDialog = active;
+    let dialog = messagesData.find(dialog => {
+        if (dialog.id === activeDialog.id)
+            return dialog;
+    });
+    dialog.messages.map(m => <Message text={m}/>);
 }
 
 const MessagesPage = () => {
+    chooseActiveDialog();
     return (
         <div className={styles.wrapper}>
-            <div className={styles.dialogs}>
-                <DialogItem name={'Dima'} dialogId={0}></DialogItem>
-                <DialogItem name={'Eugene'} dialogId={1}></DialogItem>
-                <DialogItem name={'Sasha'} dialogId={2}></DialogItem>
-                <DialogItem name={'xx'} dialogId={3}></DialogItem>
+            <div className={styles.dialogs} id='dialogs'>
+                { dialogs }
             </div>
             <div className={styles.messages}>
-                <Message text={'hi'}></Message>
-                <Message text={'hi'}></Message>
-                <Message text={'hi'}></Message>
+                { messages }
             </div>
         </div>
     );
